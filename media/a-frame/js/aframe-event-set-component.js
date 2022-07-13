0874cac9103eb47b6f1310804a2d24ef3b2a19cd
Alexander David Frick <Alex313031@gmail.com>
2022-07-12 23:18:41 -0500

    Add files via upload

---
+++ media/a-frame/js/aframe-event-set-component.js
@@ -0,0 +1,204 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/* global AFRAME */
+var styleParser = AFRAME.utils.styleParser;
+
+if (typeof AFRAME === 'undefined') {
+  throw new Error('Component attempted to register before AFRAME was available.');
+}
+
+AFRAME.registerComponent('event-set', {
+  schema: {
+    default: '',
+    parse: function parse(value) {
+      return styleParser.parse(value);
+    }
+  },
+
+  multiple: true,
+
+  init: function init() {
+    this.eventHandler = null;
+    this.eventName = null;
+  },
+
+  update: function update(oldData) {
+    this.removeEventListener();
+    this.updateEventListener();
+    this.addEventListener();
+  },
+
+  remove: function remove() {
+    this.removeEventListener();
+  },
+
+  pause: function pause() {
+    this.removeEventListener();
+  },
+
+  play: function play() {
+    this.addEventListener();
+  },
+
+  /**
+   * Update source-of-truth event listener registry.
+   * Does not actually attach event listeners yet.
+   */
+  updateEventListener: function updateEventListener() {
+    var _this = this;
+
+    var data = this.data;
+    var el = this.el;
+    var event;
+    var target;
+    var targetEl;
+
+    // Set event listener using `_event`.
+    event = data._event || this.id;
+    target = data._target;
+
+    // Decide the target to `setAttribute` on.
+    targetEl = target ? el.sceneEl.querySelector(target) : el;
+
+    this.eventName = event;
+
+    var handler = function handler() {
+      var propName;
+      // Set attributes.
+      for (propName in data) {
+        if (propName === '_event' || propName === '_target') {
+          continue;
+        }
+        AFRAME.utils.entity.setComponentProperty.call(_this, targetEl, propName, data[propName]);
+      }
+    };
+
+    if (!isNaN(data._delay)) {
+      // Delay.
+      this.eventHandler = function () {
+        setTimeout(handler, parseFloat(data._delay));
+      };
+    } else {
+      this.eventHandler = handler;
+    }
+  },
+
+  addEventListener: function addEventListener() {
+    this.el.addEventListener(this.eventName, this.eventHandler);
+  },
+
+  removeEventListener: function removeEventListener() {
+    this.el.removeEventListener(this.eventName, this.eventHandler);
+  }
+});
+
+/***/ })
+
+/******/ });
+});
